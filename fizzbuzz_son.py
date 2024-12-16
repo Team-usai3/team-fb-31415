63d7aecd96e5fbd238cf8063e0149bdce5477863
imsonn94 <qhdry858@gmail.com>
2024-12-16 18:14:00 +0900

    feat: Revise the code to one line

--- fizzbuzz_son.py
+++ fizzbuzz_son.py
@@ -1,5 +1,6 @@
-for i in range(1, 1+15):
-    if i % 3 == 0 or i % 5 ==0:
-        print('fizz'*(i%3==0) + 'buzz'*(i%5==0))
-    else:
-        print(i)
+#for i in range(1, 1+15):
+#    if i % 3 == 0 or i % 5 ==0:
+#        print('fizz'*(i%3==0) + 'buzz'*(i%5==0))
+#    else:
+#        print(i)
+print(['fizzbuzz' if i % 15 ==0 else 'fizz' if i % 3 ==0 else 'buzz' if i % 5 == 0 else i for i in range(1,15+1)])
