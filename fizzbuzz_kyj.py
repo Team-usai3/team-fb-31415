7f80571e595e8e9832fe5183f30749878698ada3
Kim-Yeon-Jun <106429088+Kim-Yeon-Jun@users.noreply.github.com>
2024-12-16 18:15:35 +0900

    feat : Make condition statement 3 to 1 and repeat 20times

--- fizzbuzz_kyj.py
+++ fizzbuzz_kyj.py
@@ -1,7 +1,6 @@
-for i in range(1, 15+1):
-    if i%15==0:
-        print('fizzbuzz')
-    elif i%3==0:
-        print('fizz')
-    elif i%5==0:
-        print('buzz')
+for i in range(1, 20+1):
+    if i % 3 == 0 or i % 5 == 0:
+        result = 'fizz' * (i % 3==0) + 'buzz' * (i % 5==0)
+        print(result)
+    else:
+        print(i)
